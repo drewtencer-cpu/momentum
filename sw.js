92db78bdd78c20f416422fdfda55f39a6318c1c2
drewtencer-cpu <drewtencer@gmail.com>
2026-09-14 18:06:27 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='momentum-1-2';self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])))});self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
